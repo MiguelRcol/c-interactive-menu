84d779fd07436d72499f0891bc23fd1b2fde34ae
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-24 18:04:48 -0500

    chore: add program flow and pseudocode outline to main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+    // Program flow:
+// clear terminal
+// shows option menu
+// prompt user for option selection
+// while option != 4
+//     clear terminal
+//     check selected option and execute action
+//     wait for user input to continue
+//     clear terminal again
+//     show option menu again
+//     prompt user for option selection again
+
+}
