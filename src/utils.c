895b270da711bc8fe5dafefcb8100bd5ace2e489
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-27 00:12:28 -0500

    feat(utils): implement wait_enter function for user interaction

---
+++ src/utils.c
@@ -0,0 +1,9 @@
+#include "utils.h"
+#include <stdio.h>
+
+void wait_enter(void)
+{
+    printf("\nPress Enter to continue...");
+    getchar();  // consume leftover newline
+    getchar();  // wait for user
+}
---
+++ src/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void wait_enter(void);
+
+#endif
