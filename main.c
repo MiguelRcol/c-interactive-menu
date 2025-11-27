7d319ab1495905c3c29021da9faf2e586fb0d50d
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-26 22:19:22 -0500

    chore: added main skeleton for menu functions and switch cases for the tools

--- main.c
+++ main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "src/math.h"
+
+
+
 
 int main(void){
     // Program flow:
+    system("clear");
+    printf("----menu----\n");
+    printf("1. Math Tool\n");
+    printf("2. Age classifier\n");
+    printf("3. weather advice\n");
+    printf("4. Exit\n");
+    printf("Select an option: ");
+    scanf("%d", &option);
 // clear terminal
+while (option != 4)     
+{
+    system("clear")
+}
+    // check selected option and execute action
+    switch(option)
+    {
+        case 1:
+            math_tool();
+            break;
+        case 2:
+        age_classifier();
+            break;
+        case 3:
+            wearher_forecast();
+            break;
+        default:
+            printf("Invalid option. Please try again.\n");
+            break;
+    }
+    // wait for user input to continue
 // shows option menu
 // prompt user for option selection
 // while option != 4
