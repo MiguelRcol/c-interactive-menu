bea32f2734c987cd8c17fd71f62dcee2aa33dbc4
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-26 22:47:35 -0500

    feat: created main menu with options and main loop for utilities

--- src/math.c
+++ src/math.c
@@ -3,7 +3,24 @@
 
 // this funtion provides basic math operations
 void math_tool(void)
-{
+{   
+    int choice;
+    double a=0, b=0, result=0;
+
+    while (choice !=5)  
+    {
+        system("clear");
+
+        printf("Math Tool Menu:\n");
+        printf("1. Add\n");
+        printf("2. Subtract\n");
+        printf("3. Multiply\n");
+        printf("4. Divide\n");
+        printf("5. Back to main menu\n");
+        printf("Select an option: ");
+        scanf("%d", &choice);   
+    }
+    
     //TODO: implement math tool
     // Math Tool
     // 1. Add
