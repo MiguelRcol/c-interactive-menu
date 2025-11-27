1fcad0c76048a0209827058dc0470732ec83569d
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-26 22:43:44 -0500

    chore: added main pseudocode for math function

--- src/math.c
+++ src/math.c
@@ -5,6 +5,13 @@
 void math_tool(void)
 {
     //TODO: implement math tool
+    // Math Tool
+    // 1. Add
+    // 2. Subtract
+    // 3. Multiply
+    // 4. Divide
+    // 5. Back to main menu
+
     printf("Math tool coming soon!\n");
 }
 
