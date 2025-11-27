47d44d6651feeff947ac3f20ac59dd720dfc4777
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-26 23:01:24 -0500

    feat: created main division function with inside loop for handling logical division and case for division by 0

--- src/math.c
+++ src/math.c
@@ -50,6 +50,18 @@ void math_tool(void)
             break;
         case 4:
             /* division */
+            printf("Division selected.\n");
+             printf("Enter two numbers: ");
+             scanf("%lf %lf", &a, &b);
+             if (b != 0)
+             {
+                 result = a / b;
+                 printf("Result: %.2lf / %.2lf = %.2lf\n", a, b, result);
+             }
+             else
+             {
+                 printf("Error: Division by zero is not allowed.\n");
+             }
             break;
             case 5:
             /* back to main menu */
