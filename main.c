30803e696a9eaf4a9297a5e2f0f5c7696f3b8a9b
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2025-11-27 00:01:01 -0500

    refactor(main): remove commented program flow for clarity

--- main.c
+++ main.c
@@ -8,18 +8,6 @@ int main(void)
 {
     int option = 0;
 
-    // Program flow:
-    // clear terminal
-    // shows option menu
-    // prompt user for option selection
-    // while option != 4
-    //     clear terminal
-    //     check selected option and execute action
-    //     wait for user input to continue
-    //     clear terminal again
-    //     show option menu again
-    //     prompt user for option selection again
-
     while (option != 4)
     {
         system("clear");
